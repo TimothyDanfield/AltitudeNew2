188007f16b566c71ce6932d1a8c0f986ef2efd38
Timothy <TimothyDanfield@yahoo.com>
2023-04-02 20:29:47 -0500

    test

--- New site Altitude - Copy/app.js
+++ New site Altitude - Copy/app.js
@@ -1,76 +1,68 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-const navLogo = document.querySelector('#navbar__logo');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const navLogo = document.querySelector("#navbar__logo");
 
 // Display Mobile Menu
 const mobileMenu = () => {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 };
 
-menu.addEventListener('click', mobileMenu);
 
 // Show active menu when scrolling
 const highlightMenu = () => {
-  const elem = document.querySelector('.highlight');
-  const homeMenu = document.querySelector('#home-page');
-  const aboutMenu = document.querySelector('#about-page');
-  const servicesMenu = document.querySelector('#services-page');
-  const signupMenu = document.querySelector('#signup');
-  const contactUs = document.querySelector('#contact-us');
+  const elem = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#about-page");
+  const servicesMenu = document.querySelector("#services-page");
+  const signupMenu = document.querySelector("#signup");
+  const contactUs = document.querySelector("#services");
   let scrollPos = window.scrollY;
 
-  // adds 'highlight' class to my menu items
   if (window.innerWidth > 960 && scrollPos < 600) {
-    homeMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
     return;
   } else if (window.innerWidth > 960 && scrollPos < 1900) {
-    aboutMenu.classList.add('highlight');
-    homeMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    servicesMenu.classList.remove("highlight");
     return;
   } else if (window.innerWidth > 960 && scrollPos < 2450) {
-    servicesMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    signupMenu.classList.remove('highlight');
+    servicesMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    signupMenu.classList.remove("highlight");
     return;
   }
   else if (window.innerWidth > 960 && scrollPos < 3000) {
-    signupMenu.classList.add('highlight');
-    servicesMenu.classList.remove('highlight');
-    contactUs.classList.remove('highlight');
+    signupMenu.classList.add("highlight");
+    servicesMenu.classList.remove("highlight");
+    contactUs.classList.remove("highlight");
     return;
   }
 
   else if (window.innerWidth > 960 && scrollPos < 3800) {
-    contactUs.classList.add('highlight');
-    signupMenu.classList.remove('highlight');
+    contactUs.classList.add("highlight");
+    signupMenu.classList.remove("highlight");
     return;
   }
 
   if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
-    elem.classList.remove('highlight');
+    elem.classList.remove("highlight");
   }
-
-
-  menuLinks.addEventListener('click', highlightMenu);
-  
 };
 
 
-window.addEventListener('scroll', highlightMenu);
-window.addEventListener('click', highlightMenu);
 
 // Scroll
 
 
-window.addEventListener('scroll', reveal);
+window.addEventListener("scroll", reveal);
 
-window.addEventListener('load', reveal);
+window.addEventListener("load", reveal);
 
 function reveal() {
-  var reveals = document.querySelectorAll('.reveal');
+  var reveals = document.querySelectorAll(".reveal");
 
   for(var i = 0; i < reveals.length; i++) {
 
@@ -79,10 +71,10 @@ function reveal() {
     var revealPoint = 20;
 
     if(revealTop < windowheight - revealPoint) {
-      reveals[i].classList.add('active');
+      reveals[i].classList.add("active");
     }
     else {
-      reveals[i].classList.remove('active');
+      reveals[i].classList.remove("active");
     }
   }
 }
@@ -90,19 +82,25 @@ function reveal() {
 
 
 
-//  Close mobile Menu when clicking on a menu item
+ // Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
-  const menuBars = document.querySelector('.is-active');
+  const menuBars = document.querySelector(".is-active");
   if (window.innerWidth <= 768 && menuBars) {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.remove('active');
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
   }
 };
 
-menuLinks.addEventListener('click', hideMobileMenu);
-navLogo.addEventListener('click', hideMobileMenu);
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
 
 
-const playerContainer = document.getElementById('player-container');
-const player = playerContainer.querySelector('iframe');
-const observer = new IntersectionObserver(onIntersection, { threshold: 0.5 });
+// const playerContainer = document.getElementById("player-container");
+// const player = playerContainer.querySelector("iframe");
+// const observer = new IntersectionObserver(onIntersection, { threshold: 0.5 });
+
+window.onload = function() {
+  setTimeout(function() {
+  document.querySelector(".button").style.display = "block";
+  }, 2000);
+};
